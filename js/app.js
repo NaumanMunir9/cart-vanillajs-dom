528c5f8c286de14f251aecff573501bd8e2cd4a9
Nauman Munir <naumanmunir@gmail.com>
2020-08-09 01:06:28 +0500

    add item to cart - cartBtn

---
+++ js/app.js
@@ -0,0 +1,38 @@
+(function () {
+  const cartInfo = document.getElementById("cart-info");
+  const cart = document.getElementById("cart");
+
+  cartInfo.addEventListener("click", function () {
+    cart.classList.toggle("show-cart");
+  });
+})();
+
+// add items to the cart
+(function () {
+  const cartBtn = document.querySelectorAll(".store-item-icon");
+  cartBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+      if (event.target.parentElement.classList.contains("store-item-icon")) {
+        let fullPath = event.target.parentElement.previousElementSibling.src;
+        let pos = fullPath.indexOf("img") + 3;
+        let partPart = fullPath.slice(pos);
+
+        const item = {};
+        item.img = `img-cart${partPart}`;
+        let name =
+          event.target.parentElement.parentElement.nextElementSibling
+            .children[0].children[0].textContent;
+        item.name = name;
+
+        let price =
+          event.target.parentElement.parentElement.nextElementSibling
+            .children[0].children[1].textContent;
+        let finalPrice = price.slice(1).trim();
+
+        item.price = finalPrice;
+
+        console.log(item);
+      }
+    });
+  });
+})();
