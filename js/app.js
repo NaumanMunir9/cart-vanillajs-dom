0473445fe37ec20fa8e70b75c57a85819640b878
Nauman Munir <naumanmunir@gmail.com>
2020-08-11 20:03:49 +0500

    add item to cart with img, name, and the final price

--- js/app.js
+++ js/app.js
@@ -7,3 +7,33 @@
     cart.classList.toggle("show-cart");
   });
 })();
+
+// add items to the cart
+(function () {
+  const cartBtn = document.querySelectorAll(".store-item-icon");
+  cartBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      if (e.target.parentElement.classList.contains("store-item-icon")) {
+        let fullPath = e.target.parentElement.previousElementSibling.src;
+        let pos = fullPath.indexOf("img") + 3;
+        let partialPath = fullPath.slice(pos);
+
+        const item = {};
+        item.img = `img-cart${partialPath}`;
+        let name =
+          e.target.parentElement.parentElement.nextElementSibling.children[0]
+            .children[0].textContent;
+        item.name = name;
+
+        let price =
+          e.target.parentElement.parentElement.nextElementSibling.children[0]
+            .children[1].textContent;
+        let finalPrice = price.slice(1).trim();
+
+        item.price = finalPrice;
+
+        console.log(item);
+      }
+    });
+  });
+})();
